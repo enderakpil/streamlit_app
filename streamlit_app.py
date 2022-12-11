f4341acea3b7d5a418a18ed8abc0117c37394cb9
Ender Akpil <enderakpil@yahoo.com>
2022-12-11 12:12:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as streamlit
 import pandas
 
-my_furit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
 
 streamlit.title('My Parents New Healty Dinner')
