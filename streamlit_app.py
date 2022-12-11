c29a6df01ec576c8f9060ec22af004db81ecffce
Ender Akpil <enderakpil@gmail.com>
2022-12-11 12:17:53 +0300

    bla

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
 
-streamlit.title('My Parents New Healty Dinner')
+streamlit.title('My Parents New Healty Dinner, vowww')
 streamlit.header(' Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
