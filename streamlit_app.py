63679a0ce7b75e656ab1a3d1e72c758f2b05e973
Ender Akpil <enderakpil@yahoo.com>
2022-12-11 12:09:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@
 import streamlit as streamlit
 
 streamlit.title('My Parents New Healty Dinner')
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
